25e28d4599d9e24d588f04817869cbbf330f139d
21jun <21jun7654@gmail.com>
2020-05-19 02:50:16 +0900

    add: route send/gmail

--- oasis/route.py
+++ oasis/route.py
@@ -1,10 +1,23 @@
 from oasis import app
 from flask import send_file, render_template
+from oasis.email import smtp
+import os
 
 
 @app.route("/", endpoint="index")
 def index():
     return render_template("index.html")
+
+
+@app.route("/send/gmail", methods=["GET"])
+def email_send():
+    gmail = smtp.GmailSMTP(app.config["SENDER"], app.config["PASSWORD"])
+    subject = "Rasberry Pi Temperature"
+    meminfo = os.system("cat /proc/meminfo")
+    temp = os.system("vcgencmd measure_temp")
+    body = meminfo + '\n' + temp
+    gmail.send(subject, body, app.config['RECEIVER'])
+    return body
 
 
 @app.route("/finger", methods=["GET"])
